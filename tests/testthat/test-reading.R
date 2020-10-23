ee14f88c387f3bf9c4fc5ba18fb6c571e0d25566
muschellij2 <muschellij2@gmail.com>
2020-10-23 12:52:10 -0400

    fixing test for appveyor

--- tests/testthat/test-reading.R
+++ tests/testthat/test-reading.R
@@ -1,6 +1,10 @@
 testthat::context("Reading in CWA file from package")
-testthat::test_that("multiplication works", {
-  file = system.file("extdata", "ax3_testfile.cwa.gz", package = "cwaconvert")
+testthat::test_that("Reading in the data", {
+  gz_file = system.file("extdata", "ax3_testfile.cwa.gz", package = "cwaconvert")
+  file = tempfile(fileext = ".cwa")
+  file = R.utils::gunzip(gz_file, destname = file,
+                         temporary = TRUE, remove = FALSE)
+
   out = read_cwa(file)
   testthat::expect_named(
     out,
