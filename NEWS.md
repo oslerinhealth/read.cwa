221fa83faf715065abf9aba83582b7b521820d3f
muschellij2 <muschellij2@gmail.com>
2020-10-23 11:20:55 -0400

    added tests

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,4 @@
 # cwaconvert 0.1.0
 
 * Added a `NEWS.md` file to track changes to the package.
+* Added the reading and converting functions.
---
+++ tests/testthat/test-reading.R
@@ -0,0 +1,25 @@
+testthat::context("Reading in CWA file from package")
+testthat::test_that("multiplication works", {
+  file = system.file("extdata", "ax3_testfile.cwa.gz", package = "cwaconvert")
+  out = read_cwa(file)
+  testthat::expect_named(
+    out,
+    c("timestamp", "x", "y", "z", "light", "temperature", "battery",
+      "battery_voltage", "battery_percentage", "battery_relative",
+      "events")
+    )
+  testthat::expect_equal(
+    colMeans(out[, c("x", "y", "z")]),
+    c(x = 0.777613146551724, y = 0.127438936781609, z = 0.291899245689655)
+  )
+  testthat::expect_silent(read_cwa(file, verbose = FALSE))
+  out = read_cwa(file, xyz_only = TRUE)
+  testthat::expect_named(
+    out,
+    c("timestamp", "x", "y", "z")
+    )
+  testthat::expect_equal(
+    colMeans(out[, c("x", "y", "z")]),
+    c(x = 0.777613146551724, y = 0.127438936781609, z = 0.291899245689655)
+  )
+})
